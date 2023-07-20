596d9ab0c08422434c06dc3dca26aca20fd3f255
adisatr7 <adisatr7@gmail.com>
2023-07-20 18:30:52 +0700

    Restructured getch()

--- utils/terminalHelper.hpp
+++ utils/terminalHelper.hpp
@@ -7,15 +7,9 @@
 
 
 /**
- * Menghandle input tombol keyboard dari user.
- * 
- * @return Karakter yang dimasukkan user dalam tipe data `char`.
+ * Matikan mode kanonik dan echo pada terminal.
  */
-int getch() {
-
-    // Variable untuk menyimpan karakter yang dimasukkan user
-    int ch;
-
+void __disableCanonicalMode() {
     // Buat struct untuk menyimpan pengaturan terminal lama dan baru
     struct termios oldSettings, newSettings;
     
@@ -31,14 +25,52 @@ int getch() {
     // Atur pengaturan terminal baru
     tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
 
-    // Reset buffer agar tombol-tombol yang ditekan sebelumnya tidak ikut terbaca
-    while (getchar() != '\n');
+    // Bug fix agar tombol yang ditekan sebelumnya tidak ikut terbaca
+    newSettings.c_cc[VMIN] = 1;     // Set jumlah karakter minimum yang harus dibaca
+    newSettings.c_cc[VTIME] = 0;    // Set waktu tunggu pembacaan karakter
+}
+
+
+/**
+ * Aktifkan mode kanonik dan echo pada terminal.
+ */
+void __enableCanonicalMode() {
+    // Buat struct untuk menyimpan pengaturan terminal lama dan baru
+    struct termios oldSettings, newSettings;
+    
+    // Dapatkan pengaturan terminal saat ini
+    tcgetattr(STDIN_FILENO, &oldSettings);
+    
+    // Salin pengaturan terminal saat ini ke pengaturan baru
+    newSettings = oldSettings;
+    
+    // Aktifkan mode kanonik dan echo untuk pembacaan karakter tunggal untuk sementara
+    newSettings.c_lflag |= (ICANON | ECHO);
+    
+    // Atur pengaturan terminal baru
+    tcsetattr(STDIN_FILENO, TCSANOW, &newSettings);
+}
+
+
+/**
+ * Menghandle input tombol keyboard dari user.
+ * 
+ * @return Karakter yang dimasukkan user dalam tipe data `char`.
+ */
+int getch() {
+
+    // Matikan mode kanonik dan echo pada terminal
+    __disableCanonicalMode();
+
+    // Variable untuk menyimpan karakter yang dimasukkan user
+    int ch;
 
     // Baca karakter dari input tanpa menunggu Enter
     ch = getchar();
     
     // Kembalikan pengaturan terminal ke semula
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
+    __enableCanonicalMode();
+    // tcsetattr(STDIN_FILENO, TCSANOW, &oldSettings);
     
     // Kembalikan karakter yang dibaca
     return ch;
