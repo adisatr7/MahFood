9b4227ca6c925132fc40822686682c41f482ab2a
adisatr7 <adisatr7@gmail.com>
2023-07-20 15:07:30 +0700

    Added super descriptive comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
+//=============================================================================
+// > Program Utama
+//----------------------------------------------------------------------------
+// File program utama. Untuk menjalankan program, cukup jalankan file ini.
+// Abaikan semua file lainnya yang berada di dalam subfolder. Adapun file
+// config.hpp yang terletak berdampingan dengan main.cpp ini yang dapat
+// Anda cek dan ubah isinya sesuai kebutuhan.
+//=============================================================================
+
+//----------------------------------------------------------------------------
+// # Import
+//----------------------------------------------------------------------------
+// Import kode-kode dari file lain yang diperlukan.
+//----------------------------------------------------------------------------
+
 #include "./components/PageComponent.hpp"
 #include "./pages/LoginPage.hpp"
 
 
+//----------------------------------------------------------------------------
+// # Page Components
+//----------------------------------------------------------------------------
+// Deklarasikan objek PageComponent yang terdapat di dalam program ini
+// disini.
+//----------------------------------------------------------------------------
+
 PageComponent loginPage = PageComponent("Login", LoginPage);
+
+
+//----------------------------------------------------------------------------
+// int main() berisi halaman yang pertama kali ditampilkan kepada user
+// ketika program dijalankan. Ubah seesuka hati, tetapi jangan sentuh
+// bagian `renderPage()`-nya!
+//----------------------------------------------------------------------------
 
 int main() {
     loginPage.renderPage();
 
     return 0;
 }
+
+//----------------------------------------------------------------------------
