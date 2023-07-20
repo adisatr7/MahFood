6c459e2961d2eee86cf2f4cc58e08215be5c117b
adisatr7 <adisatr7@gmail.com>
2023-07-20 07:25:00 +0700

    Added PageComponent struct

---
+++ pages/PageComponent.hpp
@@ -0,0 +1,38 @@
+#ifndef PAGE_HPP
+#define PAGE_HPP
+
+#include <string>
+
+using std::string;
+
+
+/**
+ * Tipe data ini digunakan untuk menyimpan informasi tentang halaman menu 
+ * aplikasi.
+ */
+struct PageComponent {
+    string name;                // Nama halaman
+    void (*pageElement)();      // Pointer menuju fungsi utk menampilkan halaman
+
+    /**
+     * Konstruktor PageComponent. Digunakan untuk membuat objek PageComponent baru. Pembuatan 
+     * objek dilakukan di `routes.hpp` karena aplikasi sudah diatur sedemikian
+     * rupa agar komponen-komponen aplikasi lebih terstruktur dan mudah dicari.
+     * 
+     * @param name Nama halaman.
+     * @param pageElement Fungsi utk menampilkan halaman.
+     */
+    PageComponent(string name, void (*pageElement)()) {
+        this->name = name;
+        this->pageElement = pageElement;
+    }
+
+    /**
+     * Menampilkan halaman.
+     */
+    void renderPage() {
+        this->pageElement();
+    }
+};
+
+#endif
