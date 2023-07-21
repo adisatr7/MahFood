8e29ec3f60f13b8ca3bf025c3ef3eeaf375fa392
adisatr7 <adisatr7@gmail.com>
2023-07-21 11:30:37 +0700

    Added items LinkedList

---
+++ data/items.hpp
@@ -0,0 +1,13 @@
+#ifndef ITEMS_HPP
+#define ITEMS_HPP
+
+#include "../components/LinkedList.hpp"
+#include "../models/Item.hpp"
+
+
+/**
+ * Daftar item yang dijual melalui aplikasi.
+ */
+LinkedList<Item> items = LinkedList<Item>();
+
+#endif
